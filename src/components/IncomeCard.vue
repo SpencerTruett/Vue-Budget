b6159625fc4cc4d1fecb929fc2d361bfa78a6b0a
Spencer Truett <srtruett1993@gmail.com>
2021-03-19 10:56:32 -0500

    created IncomeCard

---
+++ src/components/IncomeCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card>
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-text>
+      <h1>{{ displayAmount }}</h1>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["title", "netAmount"],
+  computed: {
+    displayAmount() {
+      return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+      }).format(this.netAmount);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
